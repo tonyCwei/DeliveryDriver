1ac2a4b156d816c17c5a25079775cb0ad5086e58
Chenhao Wei <chenhao.wei@csu.fullerton.edu>
2023-01-05 20:07:24 -0800

    ontiggerif

---
+++ Assets/Delivery.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+   void OnCollisionEnter2D(Collision2D other) 
+   {
+    Debug.Log("Ouch!"); 
+   }
+   
+
+   void OnTriggerEnter2D(Collider2D other) {
+      if (other.tag == "Package") {
+      Debug.Log("Package picked up"); 
+      } else if (other.tag == "Customer") {
+        Debug.Log("delivered package"); 
+      }  
+
+   }
+
+ 
+
+
+}
